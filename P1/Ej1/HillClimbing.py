9ff75e50a29578d9cffd8b1008e60511f54e0a2f
Ricardo Espantaleón <espantaleon2001@gmail.com>
2022-02-20 21:58:10 +0100

    Experimento 2 creado y realizado

--- P1/Ej1/HillClimbing.py
+++ P1/Ej1/HillClimbing.py
@@ -2,18 +2,20 @@ import random
 from TSPGenerator import generador
 from utils import get_laplace_probability
 
+
 def evaluarSolucion(datos, solucion):
     longitud = 0
     for i in range(len(solucion)):
         longitud += datos[solucion[i - 1]][solucion[i]]
     return longitud
 
+
 def obtenerMejorVecino(solucion, datos):
     ##Obtención de los vecinos
     vecinos = []
-    l=len(solucion)
+    l = len(solucion)
     for i in range(l):
-        for j in range(i+1, l):
+        for j in range(i + 1, l):
             n = solucion.copy()
             n[i] = solucion[j]
             n[j] = solucion[i]
@@ -29,6 +31,7 @@ def obtenerMejorVecino(solucion, datos):
             mejorVecino = vecino
     return mejorVecino, mejorLongitud
 
+
 """
     Debo cambiar el estado inicial para poder encontrar soluciones
     perturbar puede ser generar de manera aleatoria una nueva instancia
@@ -38,8 +41,9 @@ def obtenerMejorVecino(solucion, datos):
     encontrados de manera aleatoria
 """
 
+
 def hillClimbing(datos):
-    l=len(datos)
+    l = len(datos)
     ##Creamos una solucion aleatoria
     ciudades = list(range(l))
     solucion = []
@@ -49,78 +53,109 @@ def hillClimbing(datos):
         ciudades.remove(ciudad)
     longitud = evaluarSolucion(datos, solucion)
 
-    #print("Longitud de la ruta: ", longitud)
+    print("Longitud de la ruta: ", longitud)
     ##Obtenemos el mejor vecino hasta que no haya vecinos mejores
     vecino = obtenerMejorVecino(solucion, datos)
     while vecino[1] < longitud:
         solucion = vecino[0]
         longitud = vecino[1]
-        #print("Longitud de la ruta: ", longitud)
+        print("Longitud de la ruta: ", longitud)
         vecino = obtenerMejorVecino(solucion, datos)
 
     return solucion, longitud
 
-def main():
-    datos = [
-        [0, 400, 500, 300], # La ciudad 0 está conectada con 0, por tanto vale 0
-        [400, 0, 300, 500], # Ciudad 1
-        [500, 300, 0, 400], # Ciudad 2
-        [300, 500, 400, 0]  # Ciudad 3
-    ]
 
+def main():
     exp_len_min = 4
     exp_len_max = 20
 
+    EXECUTE_EXPERIMENT_1 = False
+    EXECUTE_EXPERIMENT_2 = True
+
     frequencies_array = []
 
-    with open("ej1_frecuencia_aparicion_valorlocal.txt", "a") as descriptor_file:
+    if EXECUTE_EXPERIMENT_1:
 
-        for n_cities in range(exp_len_min, exp_len_max + 1):
+        # Experimento 1 para generar la gráfica de comportamiento del algoritmo en función del número de ciudades
+        with open("ej1_frecuencia_aparicion_valorlocal.txt", "a") as descriptor_file:
 
-            # Voy agregando de manera aleatoria el vector de ciudades
-            datos = generador(n_cities)
+            for n_cities in range(exp_len_min, exp_len_max + 1):
 
-            mejor_s = None
+                # Voy agregando de manera aleatoria el vector de ciudades
+                datos = generador(n_cities)
 
-            s=hillClimbing(datos)
+                mejor_s = None
 
-            if mejor_s is None:
-                mejor_s = s
+                s = hillClimbing(datos)
 
-            # Realizaremos 500 iteraciones para intentar obtener la mejor solucion
-            # por probabilidad
-
-            # Limpiamos previamente frequencies para no tener valores anteriores
-
-            frequencies_array.clear()
-
-            for i in range(500):
-                s=hillClimbing(datos)
-
-                # Voy agregando las frecuencias
-                frequencies_array.append(s[1])
-
-                if s[1] < mejor_s[1]:
+                if mejor_s is None:
                     mejor_s = s
 
-                # Imprimimos el mejor valor local encontrado en cada iteracion
-                # para asi realizar el muestro de: count(minimo global/local)/n iteraciones
+                # Realizaremos 500 iteraciones para intentar obtener la mejor solucion
+                # por probabilidad
 
-                """
-                print("--------------")
+                # Limpiamos previamente frequencies para no tener valores anteriores
+
+                frequencies_array.clear()
+
+                for i in range(500):
+                    s = hillClimbing(datos)
+
+                    # Voy agregando las frecuencias
+                    frequencies_array.append(s[1])
+
+                    if s[1] < mejor_s[1]:
+                        mejor_s = s
+
+                    # Imprimimos el mejor valor local encontrado en cada iteracion
+                    # para asi realizar el muestro de: count(minimo global/local)/n iteraciones
+
+                    """
+                    print("--------------")
+                    print("Hillclimbing con " + str(len(datos)) + " ciudades")
+                    print("Solucion final: ", s[0])
+                    print("Longitud de la ruta final: ", s[1])
+                    """
+
+                    print(f"{s[1]}")
+
+                print("===================")
                 print("Hillclimbing con " + str(len(datos)) + " ciudades")
-                print("Solucion final: ", s[0])
-                print("Longitud de la ruta final: ", s[1])
-                """
+                print("Solucion final de la ruta optima encontrada: ", mejor_s[0])
+                print("Longitud de la ruta final optima encontrada: ", mejor_s[1])
 
-                print(f"{s[1]}")
+                descriptor_file.write(f"{len(datos)} {get_laplace_probability(frequencies_array, mejor_s[1])}\n")
 
-            print("===================")
-            print("Hillclimbing con " + str(len(datos)) + " ciudades")
-            print("Solucion final de la ruta optima encontrada: ",mejor_s[0])
-            print("Longitud de la ruta final optima encontrada: ",mejor_s[1])
+    if EXECUTE_EXPERIMENT_2:
 
-            descriptor_file.write(f"{len(datos)} {get_laplace_probability(frequencies_array, mejor_s[1])}\n")
+        # Experimento 2 para generar la gráfica de evolución del mejor coste, en función del número de iteraciones para un TSP dado
+        with open("ej1_mejora_longitud_dado_niteraciones.txt", "a") as descriptor_file:
+
+            n_ciudades = 30  # Número considerable para ver la mejora existente dado el número de iteraciones
+            datos = generador(n_ciudades)
+
+            # Búsqueda de la variación de la longitud, incrementando de 10 en 10 el número de iteraciones
+            # Partiendo de 10 iteraciones hasta 500 iteraciones
+
+            max_iteraciones = 100
+            incremento = 1
+
+            for i in range(1, max_iteraciones + 1, incremento):
+
+                # Limpiamos el mejor valor de longitud encontrado previamente
+                mejor_s = None
+                for j in range(i):
+                    s = hillClimbing(datos)
+
+                    if mejor_s is None:
+                        mejor_s = s
+
+                    # Almacenamos el mejor valor de longitud encontrado
+                    if s[1] < mejor_s[1]:
+                        mejor_s = s
+
+                descriptor_file.write(f"{i} {mejor_s[1]}\n")
+
 
 if __name__ == "__main__":
     main()
