0756bf3af8615662928afba57fe232e54053a96a
Ricardo Espantaleón <espantaleon2001@gmail.com>
2022-03-29 18:34:08 +0200

    Corrección sobre dataset y lectura de datos. Contabilización de frecuencias agregado

--- P3/main.py
+++ P3/main.py
@@ -4,24 +4,47 @@
 
 import random
 import re
+import time
+import functools
+import enum
+
+
+def counts_frequency_of_occurrences(patient_list: [], event: str, epoch: int) -> float:
+    """
+    Function that traverses the array of patients calculating the frequency of occurrence of an event X at time Y.
+
+    :param patient_list: List of patients with apnea
+    :param event: Event that occurs
+    :param epoch: Moment when the event occurs
+    :return: Frequency of occurrence of event X at time Y
+    """
+
+    frequency = float(0)
+
+    for patient in patient_list:
+        if event in patient.keys():
+            if epoch in patient[event]:
+                frequency += 1
+
+    return float(frequency / patient_list.__len__())
 
 
 def main():
     path = "dataset_100_500.txt"
+    list_patients = []
 
     with open(path, "r") as file:
         lines = file.readlines()
-        list_patients = []
         patient_index = 0
 
         # Procesamos línea por línea el fichero
         for line in lines:
-            regex = '[0-9]+:[A-Z]'
+            regex = '[A-Z] [0-9]+'
 
             # Por cada paciente almacenaremos un diccionario donde k = behaviour y v = lista de épocas
             dictionary_patient = {}
             for item in re.findall(regex, line):
-                epoch, behaviour = item.split(':')
+                behaviour, epoch = item.split(' ')
 
                 if behaviour in dictionary_patient.keys():
                     pass
@@ -37,8 +60,12 @@ def main():
                 for key in list_patients[index]:
                     list_patients[index][key] = frozenset(list_patients[index][key])
 
-            print(f"Paciente 0: {list_patients[0]}")
-
+    start_time = time.time()
+    occurrence = counts_frequency_of_occurrences(list_patients, 'B', 1)
+    end_time = time.time()
+    print(
+        f"La frecuencia de aparición del suceso B en la época 1 es {occurrence} ({(end_time - start_time) * 1000} ms)")
+    print(list_patients[0])
 
 if __name__ == "__main__":
     main()
